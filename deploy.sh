c33387d861a423825f363c8667706050437ff51a
Rich Wareham <rjw57@cam.ac.uk>
2014-09-02 11:11:24 +0100

    add static site deploy secret

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$STATIC_SITE_SECRET" ]; then
+  echo "ERROR: static site secret is not set" >&2
+  exit 1
+fi
+
 echo "Archiving site"
 pushd _site
 tar cvjf ../static.tar.bz2 .
